8ba58083c32e04fbbf88fe93b85ed4bfc8bfb9dc
Teufelsfrucht <denngu17@gmail.com>
2025-10-26 01:07:55 +0200

    Dennis 16,09

---
+++ LSEG.py
@@ -0,0 +1,32 @@
+import lseg.data as ld
+import pandas as pd
+from lseg.data.discovery import Chain
+import datetime
+from IPython.display import display, clear_output
+
+
+
+
+def getHistoryData(universe : list[str]   , fields: list[str] , start :datetime.datetime , end : datetime.datetime, interval : str) -> pd.DataFrame:
+    ld.open_session()
+
+    df = ld.get_history(
+        universe= universe,
+        fields=fields,
+        start=start,
+        end=end,
+        interval=interval,
+    )
+    print(df)
+
+    ld.close_session()
+
+    return df
+
+
+test = getHistoryData(universe= [".GDAXI"],
+                    fields= ["OPEN_PRC"],
+                    start= datetime.datetime(2015, 1, 1),
+                    end= datetime.datetime(2025, 10, 25) ,
+                    interval='30min')
+
